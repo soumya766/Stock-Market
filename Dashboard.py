0704f259b7a5203378094ad21e7f4c0eaecef86a
soumya766 <136911114+soumya766@users.noreply.github.com>
2023-06-17 21:02:35 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -10,8 +10,8 @@ st.title(pageTitle + " " + pageIcon)
 # ----- DROP DOWN VALUES FOR COMPANY TO SELECT FOR ANALYSIS ------
 import sqlite3
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
+#import pandas as pd
+#import matplotlib.pyplot as plt
 
 # Establish a connection to the SQLite database
 conn = sqlite3.connect('df_new.db')
